7e5bf90b525fa7340002d29c8c741aee60700a19
Shota100 <b1014048@fun.ac.jp>
2017-09-24 04:57:21 +0900

    各ページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  get 'message/index' => 'message#index'
+
+  get 'detail/index' => 'detail#index'
+
+  get 'search/index' => 'search#index'
+
+  get 'profile/index' => 'profile#index'
+
   get '/' => 'home#top'
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
