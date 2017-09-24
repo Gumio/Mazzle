583df4ae07808c40b629299804b296ddaf21ec1c
Gumio <quran.gumi3134@gmail.com>
2017-09-24 13:36:25 +0900

    gem fileの調整

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'profiles/new'
   post 'profiles/create'
   get 'profiles/show'
-
+  get 'search/show'
+  get 'search/result'
+  post 'search/result'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
